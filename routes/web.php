5beb820b526caf83039754c865ec58056a2fecd7
wldudgus2012 <wldudgus2012@gmail.com>
2019-04-21 15:27:08 +0900

    '라우팅'

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,13 @@
 
 Route::get('/', function () {
     return view('welcome');
+    //return '<h1>Hello Foo</h1>';
+});
+
+Route::get('/', ['as' => 'home', function () {
+    return '제 이름은 "home"입니다.';
+}]);
+
+Route::get('/home', function() {
+    return redirect(route('home'));
 });
